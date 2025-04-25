7ed14b0f0c2d8acc3c05445a653a65c17f8bf04d
JorgeMGuimaraes <jmguimaraes@id.uff.br>
2025-04-24 23:38:35 -0300

    fix: add typing notation

--- Platform.py
+++ Platform.py
@@ -3,11 +3,11 @@
 #-----------------------------------------------------------------------------#
 # Platform: Schema -----------------------------------------------------------#
 class Platform:
-    def __init__(self, id, name, long_name, url):
-        self.id         = id
-        self.name       = name
-        self.long_name  = long_name
-        self.url        = url
+    def __init__(self, id: int, name: str, long_name: str, url:str):
+        self.id: int         = id
+        self.name: str       = name
+        self.long_name: str  = long_name
+        self.url: str        = url
 
     def __str__(self):
         return f"Plataform #{self.id}: {self.name} ({self.long_name})"
